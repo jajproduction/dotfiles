28e1809bed3e93a865e31a976e47c7dd7d642970
Jom Dollesin <codewithjom@gmail.com>
2024-02-14 21:57:27 +0800

    feat: new qtile setup with minor updates.

---
+++ .config/qtile/autostart.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+COLORSCHEME=DoomOne
+
+### AUTOSTART PROGRAMS ###
+lxsession &
+picom -b &
+nm-applet &
+volumeicon &
+xsetroot -cursor_name left_ptr &
+sxhkd -c ~/.config/qtile/scripts/sxhkdrc &
+nitrogen --set-scaled --restore &
+xidlehook --not-when-audio --not-when-fullscreen --timer 900 'systemctl suspend;i3lock -c 000000' '' &
+flameshot &
+redshift &
