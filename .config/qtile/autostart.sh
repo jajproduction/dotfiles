cb868b5ec367506c3e2d01d5b6db9473dd001590
Jaj Dollesin <codewithjom@gmail.com>
2024-05-18 15:27:58 +0800

    feat: new updates.

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -10,8 +10,8 @@ volumeicon &
 xsetroot -cursor_name left_ptr &
 sxhkd -c ~/.config/qtile/sxhkdrc &
 # nitrogen --set-scaled --restore &
-# nitrogen --set-scaled ~/.dotfiles/wall.jpg &
-xwallpaper --stretch $HOME/.dotfiles/wall.jpg &
+nitrogen --set-scaled ~/.dotfiles/wall.png &
+# xwallpaper --stretch $HOME/.dotfiles/wall.png &
 xidlehook --not-when-audio --not-when-fullscreen --timer 1800 'systemctl suspend;i3lock -c 000000' '' &
 flameshot &
 redshift &
